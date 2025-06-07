c8658d8e3666ef303c2e05c2b554a8e4342c25c9
Asma Sahli <150928892+AsmaSahli@users.noreply.github.com>
2025-06-07 23:15:54 +0100

    commit

--- server/routes/Category.routes.js
+++ server/routes/Category.routes.js
@@ -6,7 +6,7 @@ const categoriesController = require("../controllers/categoriesController");
 router.get("/", categoriesController.getCategories);
 router.post("/", categoriesController.createCategory);
 router.delete("/:id", categoriesController.deleteCategory);
-router.put("/:id", categoriesController.updateCategory);
+router.put("/:id", categoriesController.updateCategory); 
 router.patch("/:id/subcategories", categoriesController.addSubcategory);
 
 module.exports = (app) => app.use("/api/categories", router);
